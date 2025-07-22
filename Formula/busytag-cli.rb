8ba00a2725b1944a2e34ce9b8236ff44a1b4cd0b
Kristaps Pastars <kp1994@inbox.lv>
2025-07-22 18:00:37 +0300

    Little update.

--- Formula/busytag-cli.rb
+++ Formula/busytag-cli.rb
@@ -2,7 +2,7 @@ class BusytagCli < Formula
   desc "Command-line interface for BusyTag device management"
   homepage "https://github.com/busy-tag/busytag-cli"
   url "https://github.com/busy-tag/busytag-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "a011b41aaa9aca51def6d7bb102558d204e3ee08"  # Calculate with: shasum -a 256 v1.0.0.tar.gz
+  sha256 "2bbc73145d9f8da4be5dbe08bc7765a9ced94d55f8b23989d3385ce8df10c921"  # Calculate with: shasum -a 256 v1.0.0.tar.gz
   license "MIT"
   head "https://github.com/busy-tag/busytag-cli.git", branch: "main"
 
